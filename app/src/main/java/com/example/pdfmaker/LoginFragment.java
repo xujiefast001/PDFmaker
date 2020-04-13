54af786bfccacc0e5f28df2d2a5dba667f9208b8
xjfast001 <315401414@qq.com>
2020-04-13 22:45:37 +0800

    2020.4.13(3)

--- app/src/main/java/com/example/pdfmaker/LoginFragment.java
+++ app/src/main/java/com/example/pdfmaker/LoginFragment.java
@@ -2,38 +2,40 @@ package com.example.pdfmaker;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.databinding.DataBindingComponent;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.viewbinding.ViewBinding;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LoginFragment extends Fragment {
-    private UserViewModel userViewModel;
-
+    private UserdataViewModel userdataViewModel;
+    Button button2;
     public LoginFragment() {
         // Required empty public constructor
     }
 
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-                return inflater.inflate(R.layout.fragment_login, container, false);
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        userdataViewModel=new ViewModelProvider(getActivity()).get(UserdataViewModel.class);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        
+            }
+        });
+        return inflater.inflate(R.layout.fragment_login,container,false);
     }
 }
